960d2b92aeeb4f77603c06d3cbf79f0add4a651d
Garrett Heinlen <heinleng@gmail.com>
2014-11-05 15:28:00 +1100

    hooking up to heroku end points

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,7 @@ var phonecatApp = angular.module('phonecatApp', []);
 
 phonecatApp.controller('UserListCtrl', ['$scope', '$http',
   function ($scope, $http) {
-    $http.get('http://localhost:9292/users.json').success(function(data) {
+    $http.get('https://micro-users.herokuapp.com/users.json').success(function(data) {
       $scope.users = data;
     });
 
@@ -11,9 +11,9 @@ phonecatApp.controller('UserListCtrl', ['$scope', '$http',
 
 phonecatApp.controller('SubscriptionListCtrl', ['$scope', '$http',
   function ($scope, $http) {
-    $http.get('http://localhost:9290/subscriptions.json').success(function(data) {
+    $http.get('https://micro-subscriptions.herokuapp.com/subscriptions.json').success(function(data) {
       $scope.subscriptions = data;
     });
 
-    $scope.orderProp = 'age';
+    $scope.orderProp = 'name';
   }]);
